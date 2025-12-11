15ab0671a16436ef1006df7e91c46212afdaaf90
Ignacio Cuñado Barral <ignacunado@gmail.com>
2025-12-11 13:39:33 +0100

    feat: Add simple script to download relbench tasks for later preprocessing

---
+++ download_task.py
@@ -0,0 +1,17 @@
+import argparse
+
+from relbench.datasets import get_dataset
+from relbench.tasks import get_task
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--task', type=str)
+    args = parser.parse_args()
+
+    get_dataset(args.dataset, download=True)
+    get_task(args.dataset, args.task, download=True)
+
+
+if __name__ == "__main__":
+    main()
